5280b3e7cd52d156477675a44db00da816200c8b
Veikkosuhonen <veikko@suhonen.net>
2021-06-28 02:07:11 +0300

    fixed a typo

--- user.py
+++ user.py
@@ -14,7 +14,7 @@ def get_public_user(name):
     if u == None:
         return None
     userid, join_date = u
-    shops = shop.get_shops_owner_by(userid)
+    shops = shop.get_shops_owned_by(userid)
     return  {
         "username": name,
         "userid": userid,
